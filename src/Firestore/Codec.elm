9163166290e31c74edbbe7478d6c4be87390e1e1
Unknown <orasund@gmail.com>
2020-11-23 22:01:20 +0100

    added documentation brackets

--- src/Firestore/Codec.elm
+++ src/Firestore/Codec.elm
@@ -1,42 +1,9 @@
 module Firestore.Codec exposing
-    ( Codec
-    , Document
-    , Field
-    , andThen
-    , asDecoder
-    , asEncoder
-    , bool
-    , build
-    , bytes
-    , construct
-    , decode
-    , dict
-    , document
-    , encode
-    , fail
-    , geopoint
-    , int
-    , list
-    , map
-    , maybe
-    , null
-    , optional
-    , reference
-    , required
-    , string
-    , succeed
-    , timestamp
+    ( Codec, asEncoder, encode, asDecoder, decode
+    , Document, document, build, required, optional
+    , Field, bool, bytes, int, string, list, dict, null, maybe, timestamp, geopoint, reference
+    , map, andThen, succeed, fail, construct
     )
-
-import Dict exposing (Dict)
-import Firestore.Decode as Decode exposing (Decoder)
-import Firestore.Encode as Encode exposing (Encoder)
-import Firestore.Types.Geopoint exposing (Geopoint)
-import Firestore.Types.Reference exposing (Reference)
-import Json.Decode as D
-import Json.Encode as E
-import Time exposing (Posix)
-
 
 {-| Codec for Firestore
 
@@ -58,26 +25,42 @@ import Time exposing (Posix)
 @docs map, andThen, succeed, fail, construct
 
 -}
+
+import Dict exposing (Dict)
+import Firestore.Decode as Decode exposing (Decoder)
+import Firestore.Encode as Encode exposing (Encoder)
+import Firestore.Types.Geopoint exposing (Geopoint)
+import Firestore.Types.Reference exposing (Reference)
+import Json.Decode as D
+import Json.Encode as E
+import Time exposing (Posix)
+
+
+{-| -}
 type Codec a
     = Codec (Decoder a) (a -> Encoder)
 
 
+{-| -}
 asEncoder : Codec a -> (a -> Encoder)
 asEncoder (Codec _ out) =
     out
 
 
+{-| -}
 asDecoder : Codec a -> Decoder a
 asDecoder (Codec out _) =
     out
 
 
+{-| -}
 encode : Codec a -> a -> E.Value
 encode codec =
     asEncoder codec
         >> Encode.encode
 
 
+{-| -}
 decode : Codec a -> D.Decoder a
 decode =
     asDecoder
@@ -88,20 +71,24 @@ decode =
 --------------------------------------------------------------------------------
 
 
+{-| -}
 type Document a cons
     = Document (Decoder cons) (a -> List ( String, Encode.Field ))
 
 
+{-| -}
 document : cons -> Document a cons
 document fun =
     Document (Decode.document fun) (always [])
 
 
+{-| -}
 build : Document a a -> Codec a
 build (Document d e) =
     Codec d (e >> Encode.document)
 
 
+{-| -}
 required : String -> (a -> b) -> Field b -> Document a (b -> cons) -> Document a cons
 required name getter (Field dField eField) (Document d e) =
     Document
@@ -109,6 +96,7 @@ required name getter (Field dField eField) (Document d e) =
         (\value -> ( name, getter value |> eField ) :: e value)
 
 
+{-| -}
 optional : String -> (a -> b) -> Field b -> b -> Document a (b -> cons) -> Document a cons
 optional name getter (Field dField eField) default (Document d e) =
     Document
@@ -120,60 +108,72 @@ optional name getter (Field dField eField) default (Document d e) =
 --------------------------------------------------------------------------------
 
 
+{-| -}
 type Field a
     = Field (Decode.Field a) (a -> Encode.Field)
 
 
+{-| -}
 bool : Field Bool
 bool =
     Field Decode.bool Encode.bool
 
 
+{-| -}
 bytes : Field String
 bytes =
     Field Decode.bytes Encode.bytes
 
 
+{-| -}
 int : Field Int
 int =
     Field Decode.int Encode.int
 
 
+{-| -}
 string : Field String
 string =
     Field Decode.string Encode.string
 
 
+{-| -}
 list : Field a -> Field (List a)
 list (Field d e) =
     Field (Decode.list d) (Encode.list e)
 
 
+{-| -}
 dict : Field a -> Field (Dict String a)
 dict (Field d e) =
     Field (Decode.dict d) (Encode.dict e)
 
 
+{-| -}
 null : Field ()
 null =
     Field Decode.null (always Encode.null)
 
 
+{-| -}
 maybe : Field a -> Field (Maybe a)
 maybe (Field d e) =
     Field (Decode.maybe d) (Encode.maybe e)
 
 
+{-| -}
 timestamp : Field Posix
 timestamp =
     Field Decode.timestamp Encode.timestamp
 
 
+{-| -}
 geopoint : Field Geopoint
 geopoint =
     Field Decode.geopoint Encode.geopoint
 
 
+{-| -}
 reference : Field Reference
 reference =
     Field Decode.reference Encode.reference
@@ -183,30 +183,45 @@ reference =
 --------------------------------------------------------------------------------
 
 
+{-| -}
 map : (a -> b) -> (b -> a) -> Field a -> Field b
 map to from (Field d e) =
     Field (d |> Decode.map to)
         (from >> e)
 
 
+{-| -}
 andThen : (a -> Field b) -> (b -> a) -> Field a -> Field b
 andThen to from (Field d e) =
-    Field (d |> Decode.andThen to)
+    Field
+        (d
+            |> Decode.andThen
+                (\a ->
+                    let
+                        (Field dField _) =
+                            to a
+                    in
+                    dField
+                )
+        )
         (from >> e)
 
 
+{-| -}
 succeed : a -> Field a
 succeed default =
     Field (Decode.succeed default)
         (always Encode.null)
 
 
+{-| -}
 fail : String -> Field a
 fail msg =
     Field (Decode.fail msg)
         (always Encode.null)
 
 
+{-| -}
 construct : Decoder a -> (a -> Encoder) -> Codec a
 construct d e =
     Codec d e
