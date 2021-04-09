766e4f6add369c08bfd62b5493dd25809f3d1d5f
Seiya IZUMI <IzumiSy@users.noreply.github.com>
2021-04-09 13:59:15 +0900

    Use firebase emulators (#26)

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -Ceux
+
+# npx firebase setup:emulators:firestore
+
+npx firebase emulators:exec \
+  "npx elm-test && curl -s -o /dev/null -X DELETE \"http://localhost:8080/emulator/v1/projects/firestore-integration-test/databases/(default)/documents\"" \
+  --only firestore \
+  --project firestore-integration-test
+
