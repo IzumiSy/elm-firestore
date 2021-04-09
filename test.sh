016f3dc7cd2473e78dbfc02b937c50005456cab3
IzumiSy <beetle-noise@gmx.com>
2021-04-09 19:05:55 +0900

    wip

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -Ceux
 
-# npx firebase setup:emulators:firestore
-
 npx firebase emulators:exec \
   "npx elm-test && curl -s -o /dev/null -X DELETE \"http://localhost:8080/emulator/v1/projects/firestore-integration-test/databases/(default)/documents\"" \
   --only firestore \
