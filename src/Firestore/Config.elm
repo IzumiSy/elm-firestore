b90392087a41664f3872efb910dded96c6706b78
IzumiSy <beetle-noise@gmx.com>
2021-04-09 18:36:14 +0900

    Added withHost function

--- src/Firestore/Config.elm
+++ src/Firestore/Config.elm
@@ -1,6 +1,6 @@
 module Firestore.Config exposing
     ( Config
-    , new, withAuthorization, withDatabase
+    , new, withAuthorization, withDatabase, withHost
     , endpoint, httpHeader
     )
 
@@ -11,7 +11,7 @@ module Firestore.Config exposing
 
 # Constructors
 
-@docs new, withAuthorization, withDatabase
+@docs new, withAuthorization, withDatabase, withHost
 
 
 # Extractors
@@ -29,8 +29,12 @@ type alias APIKey =
     Typed APIKeyType String Typed.ReadWrite
 
 
-type alias Project =
+type alias ProjectId =
     Typed ProjectType String Typed.ReadWrite
+
+
+type alias BaseUrl =
+    Typed BaseUrlType String Typed.ReadWrite
 
 
 {-| Data type for Firestore configuration
@@ -39,30 +43,51 @@ This type internally has all information which is required to send requests to F
 
 -}
 type Config
-    = Config APIKey Project Database (Maybe Authorization)
+    = Config
+        { apiKey : APIKey
+        , project : ProjectId
+        , database : DatabaseId
+        , authorization : Maybe Authorization
+        , baseUrl : BaseUrl
+        }
 
 
 {-| Creates a new Config
 -}
 new : { apiKey : String, project : String } -> Config
 new config =
-    Config (Typed.new config.apiKey) (Typed.new config.project) defaultDatabase Nothing
+    Config
+        { apiKey = Typed.new config.apiKey
+        , project = Typed.new config.project
+        , database = defaultDatabase
+        , authorization = Nothing
+        , baseUrl = Typed.new "https://firestore.googleapis.com"
+        }
 
 
 {-| Builds an endpoint string without path
 -}
 endpoint : List UrlBuilder.QueryParameter -> String -> Config -> String
-endpoint params path (Config apiKey_ project (Database database_) _) =
-    String.append "https://firestore.googleapis.com" <|
-        UrlBuilder.absolute
-            [ "v1beta1/projects"
-            , Typed.value project
-            , "databases"
-            , Typed.value database_
-            , "documents"
-            , path
-            ]
-            (List.append params [ UrlBuilder.string "key" (Typed.value apiKey_) ])
+endpoint params path (Config { apiKey, project, database, baseUrl }) =
+    UrlBuilder.crossOrigin
+        (Typed.value baseUrl)
+        [ "v1beta1/projects"
+        , Typed.value project
+        , "databases"
+        , Typed.value database
+        , "documents"
+        , path
+        ]
+        (List.append params [ UrlBuilder.string "key" (Typed.value apiKey) ])
+
+
+
+-- Host
+
+
+withHost : String -> Int -> Config -> Config
+withHost host port_ (Config config) =
+    Config { config | baseUrl = Typed.new (host ++ ":" ++ String.fromInt port_) }
 
 
 
@@ -79,15 +104,15 @@ type alias Authorization =
 
 -}
 withAuthorization : String -> Config -> Config
-withAuthorization value (Config apiKey_ project_ database_ _) =
-    Config apiKey_ project_ database_ (Just <| Typed.new value)
+withAuthorization value (Config config) =
+    Config { config | authorization = Just <| Typed.new value }
 
 
 {-| Extracts authorization with bearer prefix as `Http.Header`.
 -}
 httpHeader : Config -> List Http.Header
-httpHeader (Config _ _ _ maybeAuthorization) =
-    maybeAuthorization
+httpHeader (Config { authorization }) =
+    authorization
         |> Maybe.map Typed.value
         |> Maybe.map (Http.header "Bearer")
         |> Maybe.map List.singleton
@@ -98,19 +123,15 @@ httpHeader (Config _ _ _ maybeAuthorization) =
 -- Database
 
 
-type alias DatabaseID =
-    Typed DatabaseIDType String Typed.ReadWrite
-
-
-type Database
-    = Database DatabaseID
+type alias DatabaseId =
+    Typed DatabaseIdType String Typed.ReadWrite
 
 
 {-| Specifies database ID to connect to.
 -}
 withDatabase : String -> Config -> Config
-withDatabase value (Config apiKey_ project_ _ authorization_) =
-    Config apiKey_ project_ (Database <| Typed.new value) authorization_
+withDatabase value (Config config) =
+    Config { config | database = Typed.new value }
 
 
 
@@ -129,10 +150,14 @@ type AuthorizationType
     = AuthorizationType
 
 
-type DatabaseIDType
-    = DatabaseIDType
+type BaseUrlType
+    = BaseUrlType
 
 
-defaultDatabase : Database
+type DatabaseIdType
+    = DatabaseIdType
+
+
+defaultDatabase : DatabaseId
 defaultDatabase =
-    Database <| Typed.new "(default)"
+    Typed.new "(default)"
