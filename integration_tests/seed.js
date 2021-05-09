e37d2e407a30c72bde0f82bdef426de7402a56a1
IzumiSy <beetle-noise@gmx.com>
2021-05-09 10:50:49 +0900

    Removed unnecessary lines

--- integration_tests/seed.js
+++ integration_tests/seed.js
@@ -25,23 +25,3 @@ class Seed {
 }
 
 module.exports = Seed;
-
-/*
-firebase.initializeApp({ projectId: "firestore-integration-test" })
-firebase.firestore().useEmulator("localhost", 8080)
-
-exports.loadSeeds = () => {
-  const db = firebase.firestore()
-  return Promise.all(seeds.users.map(async (value, index) => {
-    await db.collection("users").doc(`user${index}`).set(value)
-    return Promise.all(seeds.extras[`user${index}`].map(value => {
-      return db.collection("users").doc(`user${index}`).collection("extras").add(value)
-    }))
-  }))
-}
-
-exports.clearAll = async () => {
-  const users = await firebase.firestore().collection("users").get()
-  return Promise.all(users.docs.map(doc => doc.ref.delete()))
-}
-*/
