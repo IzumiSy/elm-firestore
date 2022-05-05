203d9c534153137482719272b1312a206585e09e
IzumiSy <beetle-noise@gmx.com>
2022-05-05 12:30:45 +0900

    Added some more errors

--- src/Firestore/Internals/Path.elm
+++ src/Firestore/Internals/Path.elm
@@ -12,8 +12,12 @@ module Firestore.Internals.Path exposing
 import List.Nonempty as NEList
 
 
+type alias SubCollectionDepth =
+    Int
+
+
 type Path
-    = Path (List Element)
+    = Path (List Element) SubCollectionDepth
 
 
 {-| Elements that constructs a path
@@ -33,17 +37,17 @@ type Element
 
 new : Path
 new =
-    Path []
+    Path [] 0
 
 
 addCollection : String -> Path -> Path
-addCollection e (Path path) =
-    Path <| path ++ List.singleton (Collection e)
+addCollection e (Path path depth) =
+    Path (path ++ List.singleton (Collection e)) (depth + 1)
 
 
 addDocument : String -> Path -> Path
-addDocument e (Path path) =
-    Path <| path ++ List.singleton (Document e)
+addDocument e (Path path depth) =
+    Path (path ++ List.singleton (Document e)) depth
 
 
 
@@ -52,17 +56,24 @@ addDocument e (Path path) =
 
 type Error
     = InvalidCharacterContained
+    | TooLongBytes
+    | TooDeepSubCollections
 
 
+{-| Validates a path
+
+This function follows the document provided at: <https://firebase.google.com/docs/firestore/quotas#collections_documents_and_fields>
+
+-}
 validate : Path -> Result (NEList.Nonempty Error) Path
-validate (Path elements) =
+validate (Path elements depth) =
     elements
         |> List.map validateElement
         |> List.foldl
             (\next current ->
                 case ( next, current ) of
                     ( Ok _, Ok _ ) ->
-                        Ok <| Path elements
+                        Ok <| Path elements depth
 
                     ( Ok _, Err err ) ->
                         Err err
@@ -73,19 +84,41 @@ validate (Path elements) =
                     ( Err err, Err errs ) ->
                         Err <| NEList.cons err errs
             )
-            (Ok <| Path elements)
+            (if depth > 100 then
+                Err <| NEList.singleton TooDeepSubCollections
+
+             else
+                Ok <| Path elements depth
+            )
 
 
 validateElement : Element -> Result Error Element
-validateElement e =
-    Ok e
+validateElement element =
+    let
+        value =
+            unwrapElement element
+    in
+    if String.length value > 1500 then
+        Err TooLongBytes
+
+    else if String.contains "/" value then
+        Err InvalidCharacterContained
+
+    else
+        Ok element
 
 
 errorString : Error -> String
-errorString e =
-    case e of
+errorString err =
+    case err of
         InvalidCharacterContained ->
             "invalid character contained"
+
+        TooLongBytes ->
+            "collection/document ID is too long (it must be under 1500 bytes)"
+
+        TooDeepSubCollections ->
+            "subcollections are too deep (it must not be over 100)"
 
 
 
@@ -93,7 +126,7 @@ errorString e =
 
 
 toString : Path -> String
-toString (Path path) =
+toString (Path path _) =
     path
         |> List.map unwrapElement
         |> String.join "/"
