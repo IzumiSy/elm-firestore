d00679f240aa86939027cd8f0dd6567b955deadb
IzumiSy <beetle-noise@gmx.com>
2022-05-08 16:59:16 +0900

    Pipeline style

--- src/Firestore/Encode.elm
+++ src/Firestore/Encode.elm
@@ -1,17 +1,17 @@
 module Firestore.Encode exposing
-    ( Encoder, encode, Field
-    , document, merge
+    ( Encoder, encode
+    , Builder, new, field, Field, build, merge
     , bool, bytes, int, string, list, dict, null, maybe, timestamp, geopoint, reference
     )
 
 {-| Encoders for Firestore
 
-@docs Encoder, encode, Field
+@docs Encoder, encode
 
 
 # Constructors
 
-@docs document, merge
+@docs Builder, new, field, Field, build, merge
 
 
 # Types
@@ -35,7 +35,7 @@ This can be encoded into `Json.Value` through `encode` function.
 
 -}
 type Encoder
-    = Encoder (List ( String, InternalTypes.ValidatedField ))
+    = Encoder (Dict.Dict String InternalTypes.ValidatedField)
 
 
 {-| Generates Json.Encode.Value from Encoder
@@ -43,8 +43,17 @@ type Encoder
 encode : Encoder -> JsonEncode.Value
 encode (Encoder fields) =
     fields
-        |> List.map (\( key, InternalTypes.ValidatedField field ) -> ( key, field ))
+        |> Dict.toList
+        |> List.map (\( key, InternalTypes.ValidatedField field_ ) -> ( key, field_ ))
         |> JsonEncode.object
+
+
+
+-- Constructors
+
+
+type Builder a
+    = Builder (Dict.Dict String InternalTypes.ValidatedField)
 
 
 {-| A field identifier type for Firestore encoder
@@ -53,35 +62,44 @@ type Field a
     = Field JsonEncode.Value
 
 
-
--- Constructors
-
-
-{-| Creates a new encoder
-
-This function works like `Encode.object` but accepts a list of tuples which has only encoders provided from `Firestore.Encode` module
-
-    Firestore.Encode.document
-        [ ( "name", Firestore.Encode.string "IzumiSy" )
-        , ( "age", Firestore.Encode.int 26 )
-        , ( "canCode", Firestore.Encode.bool True )
-        ]
-
+{-| Initializes a new builder for encoders
 -}
-document : List ( String, Field a ) -> Encoder
-document =
-    Encoder
-        << List.map (\( key, Field field ) -> ( key, InternalTypes.ValidatedField field ))
+new : Builder Empty
+new =
+    Builder Dict.empty
+
+
+field : String -> (a -> Field b) -> a -> Builder c -> Builder Building
+field name encoder value (Builder encoders) =
+    let
+        (Field field_) =
+            encoder value
+    in
+    Builder <| Dict.insert name (InternalTypes.ValidatedField field_) encoders
+
+
+{-| Generates an encoder from builders
+-}
+build : Builder Building -> Encoder
+build (Builder value) =
+    Encoder value
 
 
 {-| Merges encoders
 
-This function simply merges two encoders into one. It does not deal with field duplication.
+This function simply merges two encoders into one. Resolution of duplicated key is unpredictable.
 
 -}
-merge : Encoder -> Encoder -> Encoder
-merge (Encoder a) (Encoder b) =
-    Encoder <| a ++ b
+merge : Builder Building -> Builder Building -> Builder Building
+merge (Builder a) (Builder b) =
+    Builder <|
+        Dict.merge
+            (\key value -> Dict.insert key value)
+            (\key _ value -> Dict.insert key value)
+            (\key value -> Dict.insert key value)
+            a
+            b
+            Dict.empty
 
 
 
@@ -161,3 +179,11 @@ reference =
 unfield : Field a -> JsonEncode.Value
 unfield (Field value) =
     value
+
+
+type Building
+    = Building
+
+
+type Empty
+    = Empty
